51e13b9b5f4101c605d1831cf1bc34dff92b7146
schmolli <martin.schmollinger@gmail.com>
2017-09-29 04:26:29 -0700

    Initial project with first content

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Martin Schmollinger'
+SITENAME = u'Business Process Management Lab'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Reutlingen University', 'https://www.reutlingen-university.de/en/home/'),
+         ('Herman-Hollerith-Zentrum', 'http://www.hhz.de'),
+         ('BPM research @ HHZ', 'http://www.hhz.de/en/research/research-groups/business-process-management/'),('Educama', 'http://www.educama.org/'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
